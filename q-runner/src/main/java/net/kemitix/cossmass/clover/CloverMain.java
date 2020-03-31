8c440c37631331b26360304f63b55c0e63247f45
Paul Campbell <pcampbell@kemitix.net>
2020-03-31 19:31:50 +0100

    Run app using a proper Executor rather than a raw Thread

--- q-runner/src/main/java/net/kemitix/cossmass/clover/CloverMain.java
+++ q-runner/src/main/java/net/kemitix/cossmass/clover/CloverMain.java
@@ -4,6 +4,7 @@ import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import java.util.concurrent.Executors;
 
 @ApplicationScoped
 public class CloverMain {
@@ -14,14 +15,15 @@ public class CloverMain {
         this.service = service;
     }
 
-    void onStart(@Observes final StartupEvent ev) {
-        new Thread(() -> {
-            try {
-                service.run();
-            } finally {
-                System.exit(0);
-            }
-        }).start();
+    void onStart(@Observes final StartupEvent ev) throws InterruptedException {
+        Executors.newSingleThreadExecutor()
+                .submit(() -> {
+                    try {
+                        service.run();
+                    } finally {
+                        System.exit(0);
+                    }
+                });
     }
 
 }
