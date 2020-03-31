b87fcc1634e4ea428ec8f7bce08e769b2c96809c
Paul Campbell <pcampbell@kemitix.net>
2020-03-31 21:15:38 +0100

    Cache fonts

--- clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoader.java
+++ clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoader.java
@@ -5,7 +5,11 @@ import net.kemitix.cossmass.clover.images.FontFace;
 
 import javax.enterprise.context.Dependent;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Dependent
@@ -15,7 +19,21 @@ public class FontLoader {
             Logger.getLogger(
                     CloverImage.class.getName());
 
+    private final Map<File, Map<Integer, Font>> cache = new HashMap<>();
+
     public Font loadFont(final FontFace fontFace) {
+        LOGGER.info(String.format("Requesting %s %d",
+                fontFace.getFont(), fontFace.getSize()));
+        return cache.computeIfAbsent(
+                fontFace.getFont(),
+                file ->
+                        Collections.singletonMap(
+                                fontFace.getSize(),
+                                loadSizedFont(fontFace)))
+                .get(fontFace.getFont());
+    }
+
+    private Font loadSizedFont(final FontFace fontFace) {
         LOGGER.info(String.format("Loading %s %d",
                 fontFace.getFont(), fontFace.getSize()));
         try {
