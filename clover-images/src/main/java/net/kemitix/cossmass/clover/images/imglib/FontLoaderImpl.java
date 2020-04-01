c3b54dac6fdde39adf8350e1e425befed401520a
Paul Campbell <pcampbell@kemitix.net>
2020-04-01 08:41:10 +0100

    Enable ligatures, kerning and loose tracking

--- clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoaderImpl.java
+++ clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoaderImpl.java
@@ -5,7 +5,10 @@ import net.kemitix.cossmass.clover.images.FontFace;
 
 import javax.enterprise.context.Dependent;
 import java.awt.*;
+import java.awt.font.TextAttribute;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Dependent
@@ -18,8 +21,13 @@ public class FontLoaderImpl implements FontLoader {
     @Override
     public Font loadFont(final FontFace fontFace) {
         LOGGER.info(String.format("Loading %s", fontFace.getFont()));
+        final Map<TextAttribute, Object> map = new HashMap<>();
+        map.put(TextAttribute.LIGATURES, TextAttribute.LIGATURES_ON);
+        map.put(TextAttribute.KERNING, TextAttribute.KERNING_ON);
+        map.put(TextAttribute.TRACKING, TextAttribute.TRACKING_LOOSE);
         try {
-            return Font.createFont(Font.TRUETYPE_FONT, fontFace.getFont());
+            return Font.createFont(Font.TRUETYPE_FONT, fontFace.getFont())
+                    .deriveFont(map);
         } catch (final FontFormatException | IOException e) {
             throw new FatalCloverError("Font load error", e);
         }
