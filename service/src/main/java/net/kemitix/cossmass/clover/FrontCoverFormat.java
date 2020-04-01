6f3db3d1848f5993d11765237c899f9c02ce88f9
Paul Campbell <pcampbell@kemitix.net>
2020-04-01 12:12:13 +0100

    User front cover x offset for author list

--- service/src/main/java/net/kemitix/cossmass/clover/FrontCoverFormat.java
+++ service/src/main/java/net/kemitix/cossmass/clover/FrontCoverFormat.java
@@ -93,7 +93,7 @@ public abstract class FrontCoverFormat extends CloverFormat {
             return image
                     .withText(issue.getAuthors(),
                             XY.at(
-                                    issue.getAuthorsXOffset(),
+                                    issue.getAuthorsXOffset() + frontPageXOffset(),
                                     issue.getAuthorsYOffset()),
                             fontFace);
         };
