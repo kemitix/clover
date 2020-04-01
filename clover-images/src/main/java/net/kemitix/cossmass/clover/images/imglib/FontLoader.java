792f840345d88a0863a80c1ec3b2581c133b58dd
Paul Campbell <pcampbell@kemitix.net>
2020-04-01 06:54:06 +0100

    Extract interface for FontLoader

--- clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoader.java
+++ clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoader.java
@@ -1,29 +1,9 @@
 package net.kemitix.cossmass.clover.images.imglib;
 
-import net.kemitix.cossmass.clover.images.FatalCloverError;
 import net.kemitix.cossmass.clover.images.FontFace;
 
-import javax.enterprise.context.Dependent;
 import java.awt.*;
-import java.io.IOException;
-import java.util.logging.Logger;
 
-@Dependent
-public class FontLoader {
-
-    private static final Logger LOGGER =
-            Logger.getLogger(
-                    FontLoader.class.getName());
-
-    public Font loadFont(final FontFace fontFace) {
-        LOGGER.info(String.format("Loading %s %d",
-                fontFace.getFont(), fontFace.getSize()));
-        try {
-            return Font.createFont(Font.TRUETYPE_FONT, fontFace.getFont())
-                    .deriveFont(Font.PLAIN, fontFace.getSize());
-        } catch (final FontFormatException | IOException e) {
-            throw new FatalCloverError("Font load error", e);
-        }
-    }
-
+public interface FontLoader {
+    Font loadFont(FontFace fontFace);
 }
---
+++ clover-images/src/main/java/net/kemitix/cossmass/clover/images/imglib/FontLoaderImpl.java
@@ -0,0 +1,30 @@
+package net.kemitix.cossmass.clover.images.imglib;
+
+import net.kemitix.cossmass.clover.images.FatalCloverError;
+import net.kemitix.cossmass.clover.images.FontFace;
+
+import javax.enterprise.context.Dependent;
+import java.awt.*;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Dependent
+public class FontLoaderImpl implements FontLoader {
+
+    private static final Logger LOGGER =
+            Logger.getLogger(
+                    FontLoaderImpl.class.getName());
+
+    @Override
+    public Font loadFont(final FontFace fontFace) {
+        LOGGER.info(String.format("Loading %s %d",
+                fontFace.getFont(), fontFace.getSize()));
+        try {
+            return Font.createFont(Font.TRUETYPE_FONT, fontFace.getFont())
+                    .deriveFont(Font.PLAIN, fontFace.getSize());
+        } catch (final FontFormatException | IOException e) {
+            throw new FatalCloverError("Font load error", e);
+        }
+    }
+
+}
