261f178e70e62bd875554cfbf70c31e6f9ffbf61
Paul Campbell <pcampbell@kemitix.net>
2020-05-07 08:36:46 +0100

    add logging and minimum cover width at 1000

--- service/src/main/java/net/kemitix/clover/service/Dimensions.java
+++ service/src/main/java/net/kemitix/clover/service/Dimensions.java
@@ -1,6 +1,7 @@
 package net.kemitix.clover.service;
 
 import lombok.Getter;
+import lombok.extern.java.Log;
 import net.kemitix.clover.spi.CloverProperties;
 import net.kemitix.clover.spi.images.Image;
 import net.kemitix.clover.spi.images.Region;
@@ -9,6 +10,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+@Log
 @Getter
 @ApplicationScoped
 public class Dimensions {
@@ -53,12 +55,14 @@ public class Dimensions {
         Region kindleCover = Region.builder()
                 .width((int) (kindleWidthInches * dpi))
                 .height((int) (kindleHeightInches * dpi)).build();
+        isBetween(widthFrontCoverOriginal, 1000, fullImageOriginal.getWidth());
         scaleFromOriginal = kindleCover.getWidth() / widthFrontCoverOriginal;
         scaledCoverArt = fullImageOriginal.toBuilder()
                 .width((int) (fullImageOriginal.getWidth() * scaleFromOriginal))
                 .height((int) (fullImageOriginal.getHeight() * scaleFromOriginal))
                 .build();
 
+        log.info("Select front cover region on scaled cover art");
         Region frontRegion = kindleCover.toBuilder()
                 .top((int) (topFrontCoverOriginal * scaleFromOriginal))
                 .left((int) (leftFrontCoverOriginal * scaleFromOriginal)).build();
@@ -67,18 +71,21 @@ public class Dimensions {
         int spineWidth = (int) (spineWidthInches * dpi);
 
         // backCrop is relative to scaledCoverArt
+        log.info("Select back cover region on scaled cover art");
         Region backRegion = frontRegion.toBuilder()
                 .left(frontRegion.getLeft() - spineWidth - frontRegion.getWidth())
                 .build();
         scaledCoverArt.mustContain(backRegion);
 
         // wrapCrop is relative to scaledCoverArt
+        log.info("Select spine region on scaled cover art");
         wrapCrop = backRegion.toBuilder()
                 .width(backRegion.getWidth() + spineWidth + frontRegion.getWidth())
                 .build();
         scaledCoverArt.mustContain(wrapCrop);
 
         // frontCrop is relative to backCrop
+        log.info("Select front cover on wrap cover");
         frontCrop = Region.builder()
                 .top(0)
                 .left(kindleCover.getWidth() + spineWidth)
@@ -87,9 +94,21 @@ public class Dimensions {
         wrapCrop.mustContain(frontCrop);
 
         // spineCrop is relative to backCrop
+        log.info("Select spine are on wrap cover");
         spineCrop = frontCrop.toBuilder()
                 .left(frontCrop.getLeft() - spineWidth)
                 .width(spineWidth).build();
         wrapCrop.mustBeBiggerThan(spineCrop);
+    }
+
+    private void isBetween(float value, int min, int max) {
+        if (value < min) {
+            throw new IllegalStateException(String.format(
+                    "Value %f is below minimum %d", value, min));
+        }
+        if (value > max) {
+            throw new IllegalStateException(String.format(
+                    "Value %f is above maximum %d", value, max));
+        }
     }
 }
