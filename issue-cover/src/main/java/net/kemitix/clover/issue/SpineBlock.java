ea3eb6afb203bdd2e7835cf2cd2c238e00421332
Paul Campbell <pcampbell@kemitix.net>
2020-05-15 10:16:44 +0100

    issue: SpineBlock use Drawable.draw

--- issue-cover/src/main/java/net/kemitix/clover/issue/SpineBlock.java
+++ issue-cover/src/main/java/net/kemitix/clover/issue/SpineBlock.java
@@ -7,7 +7,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import java.awt.*;
-import java.util.Comparator;
 import java.util.logging.Logger;
 
 @ApplicationScoped
@@ -29,13 +28,12 @@ public class SpineBlock implements Block<Graphics2D> {
         LOGGER.info("Draw elements " + spineElements.toString());
         try {
             LOGGER.info("Collect elements...");
-            spineElements.stream()
-                    .sorted(Comparator.comparing(Element::getPriority))
-                    .forEach(element -> element.draw(drawable));
+            Drawable.draw(spineElements, drawable);
         } catch (IllegalAccessError e) {
             e.printStackTrace();
         } finally {
             LOGGER.info("Finally");
         }
     }
+
 }
