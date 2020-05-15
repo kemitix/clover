2918ac0d5d557457726d9a904639e8ed4190be21
Paul Campbell <pcampbell@kemitix.net>
2020-05-15 10:15:39 +0100

    spi.Drawable: add helper to draw Drawables in correct order

--- spi/src/main/java/net/kemitix/clover/spi/Prioritised.java
+++ spi/src/main/java/net/kemitix/clover/spi/Prioritised.java
@@ -1,5 +1,12 @@
 package net.kemitix.clover.spi;
 
+import java.util.Comparator;
+
 public interface Prioritised {
     int getPriority();
+
+    static Comparator<Prioritised> byPriority() {
+        return Comparator.comparing(Prioritised::getPriority).reversed();
+    }
+
 }
