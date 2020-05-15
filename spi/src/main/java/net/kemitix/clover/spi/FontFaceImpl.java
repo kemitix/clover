f075f3a65f16c6c8642f47673df745f4df80ec29
Paul Campbell <pcampbell@kemitix.net>
2020-05-15 12:43:58 +0100

    spi.FontFaceImpl: Vetoe as a bean

--- spi/src/main/java/net/kemitix/clover/spi/FontFaceImpl.java
+++ spi/src/main/java/net/kemitix/clover/spi/FontFaceImpl.java
@@ -3,18 +3,22 @@ package net.kemitix.clover.spi;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.enterprise.inject.Vetoed;
 import java.io.File;
 
+@Vetoed
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public class FontFaceImpl implements FontFace {
 
-    private final File font;
-    private final int size;
-    private final String colour;
-    private final String shadowColour;
-    private final XY shadowOffset;
+    private File font;
+    private int size;
+    private String colour;
+    private String shadowColour;
+    private XY shadowOffset;
 
     static String shadowColour(final String colour) {
         switch (colour) {
