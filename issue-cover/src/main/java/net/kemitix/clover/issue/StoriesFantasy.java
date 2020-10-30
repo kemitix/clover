e21134141301cd572c996ef659a3ceb6e9e54724
Paul Campbell <pcampbell@kemitix.net>
2020-10-30 18:52:17 +0000

    issue-cover.StoriesFantasy: add underline for header

--- issue-cover/src/main/java/net/kemitix/clover/issue/StoriesFantasy.java
+++ issue-cover/src/main/java/net/kemitix/clover/issue/StoriesFantasy.java
@@ -1,17 +1,29 @@
 package net.kemitix.clover.issue;
 
 import lombok.Getter;
-import net.kemitix.clover.spi.*;
+import net.kemitix.clover.spi.AbstractElement;
+import net.kemitix.clover.spi.BackCover;
+import net.kemitix.clover.spi.BackCoverBackgroundBox;
+import net.kemitix.clover.spi.IssueConfig;
+import net.kemitix.clover.spi.OpaqueFill;
+import net.kemitix.clover.spi.Region;
+import net.kemitix.clover.spi.SimpleTextEffect;
+import net.kemitix.clover.spi.StoryListFormatter;
+import net.kemitix.clover.spi.TextEffect;
+import net.kemitix.fontface.FontCache;
 import net.kemitix.fontface.FontFace;
 import net.kemitix.properties.typed.TypedProperties;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 @BackCover
 @ApplicationScoped
 public class StoriesFantasy extends AbstractElement {
+
+    private static final String HEADER = "Original Fantasy";
 
     @Getter private final int priority = 10;
 
@@ -19,6 +31,9 @@ public class StoriesFantasy extends AbstractElement {
     @Inject SimpleTextEffect<Graphics2D> simpleTextEffect;
     @Inject StoryListFormatter storyListFormatter;
     @Inject IssueConfig issueConfig;
+    @Inject OpaqueFill<Graphics2D> opaqueFill;
+    @Inject FontCache fontCache;
+    @Inject BackCoverBackgroundBox backgroundBox;
 
     @Override
     public void draw(Graphics2D drawable, TypedProperties typedProperties) {
@@ -28,6 +43,39 @@ public class StoriesFantasy extends AbstractElement {
                 .hAlign(issueConfig.getStoriesAlignment())
                 .region(region())
                 .accept(drawable);
+        opaqueFill.opacity(1d)
+                .colour(fontFace.getColour())
+                .region(hRuleRegion(drawable))
+                .accept(drawable);
+    }
+
+    private Region hRuleRegion(Graphics2D drawable) {
+        int width = lineWidth(drawable);
+        Region region = region()
+                .withTop(top -> top + lineHeight(drawable))
+                .withHeight(backgroundBox.getMarginStep())
+                .withWidth(width);
+        if (issueConfig.getStoriesAlignment()
+                .equals(TextEffect.HAlignment.CENTRE)) {
+            return region
+                    .withLeft(left -> left - (width / 2));
+        } else {
+            return region;
+        }
+    }
+
+    private int lineWidth(Graphics2D drawable) {
+        Font font = fontCache.loadFont(fontFace);
+        Rectangle2D stringBounds =
+                font.getStringBounds(HEADER, drawable.getFontRenderContext());
+        return (int) stringBounds.getWidth();
+    }
+
+    private int lineHeight(Graphics2D drawable) {
+        Font font = fontCache.loadFont(fontFace);
+        Rectangle2D stringBounds =
+                font.getStringBounds(HEADER, drawable.getFontRenderContext());
+        return (int) (stringBounds.getHeight() * 1.2);
     }
 
     private Region region() {
@@ -40,7 +88,7 @@ public class StoriesFantasy extends AbstractElement {
     private String text() {
         return String.join("\n",
                 storyListFormatter.format(
-                        "Original Fantasy",
+                        HEADER,
                         issueConfig.getStories().getFantasy()));
     }
 
