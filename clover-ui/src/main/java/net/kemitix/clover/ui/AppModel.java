28bdc7f58c8d03f135c005dda9f6dc013af595df
Paul Campbell <pcampbell@kemitix.net>
2021-03-06 18:57:23 +0000

    clover-ui: emit LoadConfigErrorEvent

--- clover-ui/src/main/java/net/kemitix/clover/ui/AppModel.java
+++ clover-ui/src/main/java/net/kemitix/clover/ui/AppModel.java
@@ -2,18 +2,22 @@ package net.kemitix.clover.ui;
 
 import javafx.application.Application;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import lombok.Getter;
+import net.kemitix.clover.service.ConfigLoadException;
 import net.kemitix.clover.service.IssueConfigLoader;
 import net.kemitix.clover.service.ServiceIssueConfig;
+import net.kemitix.clover.ui.fragment.main.MainFragment;
 import net.kemitix.files.FileReaderWriter;
 
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AppModel {
+
+    private final MainFragment mainFragment;
 
     @Getter
     private final SimpleStringProperty issueDirectoryProperty;
@@ -21,9 +25,14 @@ public class AppModel {
     private final FileReaderWriter fileReader = new FileReaderWriter();
     private final IssueConfigLoader loader = new IssueConfigLoader();
 
-    private ServiceIssueConfig serviceIssueConfig;
+    private final AtomicReference<ServiceIssueConfig> serviceIssueConfig =
+            new AtomicReference<>();
 
-    public AppModel(Application.Parameters parameters) {
+    public AppModel(
+            Application.Parameters parameters,
+            MainFragment mainFragment
+    ) {
+        this.mainFragment = mainFragment;
         List<String> unnamed = parameters.getUnnamed();
         if (unnamed.isEmpty()) {
             throw new RuntimeException("Issue Directory not specified");
@@ -32,17 +41,26 @@ public class AppModel {
 
         issueDirectoryProperty = new SimpleStringProperty();
         issueDirectoryProperty.addListener((observable, oldValue, newValue) ->
-                serviceIssueConfig = load(newValue));
+                load(newValue).ifPresent(serviceIssueConfig::set));
         issueDirectoryProperty.setValue(initialIssueDirectory);
 
     }
 
-    private ServiceIssueConfig load(String newValue) {
+    private Optional<ServiceIssueConfig> load(String newValue) {
         File file = Paths.get(newValue).resolve("clover.yaml").toFile();
-        return loader.parseYamlFromFile(
-                file,
-                ServiceIssueConfig.class,
-                fileReader);
+        try {
+            return Optional.of(loader.parseYamlFromFile(
+                    file,
+                    ServiceIssueConfig.class,
+                    fileReader));
+        } catch (ConfigLoadException e) {
+            emit(AppEvent.loadConfigError(e));
+        }
+        return Optional.empty();
+    }
+
+    private void emit(AppEvent appEvent) {
+        mainFragment.emit(appEvent);
     }
 
 }
