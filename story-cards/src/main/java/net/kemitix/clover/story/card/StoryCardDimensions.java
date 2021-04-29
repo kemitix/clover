a8cd2220c1b489dc4b3a68c2bb00b414eeb0286a
Paul Campbell <pcampbell@kemitix.net>
2021-04-29 20:06:21 +0100

    story-cards: crop and scale correctly

--- story-cards/src/main/java/net/kemitix/clover/story/card/StoryCardDimensions.java
+++ story-cards/src/main/java/net/kemitix/clover/story/card/StoryCardDimensions.java
@@ -33,8 +33,8 @@ public class StoryCardDimensions {
         sourceRegion = Region.builder()
                 .top(storyCardSpec.getTop())
                 .left(storyCardSpec.getLeft())
-                .width(properties.getWidth())
-                .height((int) (targetSizeRegion.getHeight() * scaleFromOriginal))
+                .width(targetSizeRegion.getWidth())
+                .height(targetSizeRegion.getHeight())
                 .build();
     }
 
