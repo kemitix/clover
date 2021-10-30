ece841e59284d7fdc77da974f3a5700006d4a90a
Paul Campbell <pcampbell@kemitix.net>
2021-10-30 19:07:43 +0100

    remove some logging

--- issue-cover/src/main/java/net/kemitix/clover/issue/SpineText.java
+++ issue-cover/src/main/java/net/kemitix/clover/issue/SpineText.java
@@ -1,7 +1,12 @@
 package net.kemitix.clover.issue;
 
 import lombok.Getter;
-import net.kemitix.clover.spi.*;
+import net.kemitix.clover.spi.AbstractElement;
+import net.kemitix.clover.spi.IssueConfig;
+import net.kemitix.clover.spi.IssueDimensions;
+import net.kemitix.clover.spi.RotatedCenteredTextEffect;
+import net.kemitix.clover.spi.Spine;
+import net.kemitix.clover.spi.TextEffect;
 import net.kemitix.fontface.FontFace;
 import net.kemitix.properties.typed.TypedProperties;
 
@@ -9,15 +14,10 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.awt.*;
 import java.util.function.Supplier;
-import java.util.logging.Logger;
 
 @Spine
 @ApplicationScoped
 public class SpineText extends AbstractElement {
-
-    private static final Logger LOG =
-            Logger.getLogger(
-                    SpineText.class.getName());
 
     @Getter private final int priority = 20;
 
@@ -29,7 +29,6 @@ public class SpineText extends AbstractElement {
     @Override
     public void draw(Graphics2D graphics2D, TypedProperties typedProperties) {
         String text = spineText.get();
-        LOG.info("Draw Spine Text: " + text);
         rotatedCenteredTextEffect.fontFace(fontFace)
                 .text(text)
                 .vAlign(TextEffect.VAlignment.CENTRE)
