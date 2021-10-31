81cd24feef28281f201d147d49e5a50a0c45a39b
Paul Campbell <pcampbell@kemitix.net>
2021-10-31 12:21:17 +0000

    Drawables: remove logging

--- issue-cover/src/main/java/net/kemitix/clover/issue/AbstractBlock.java
+++ issue-cover/src/main/java/net/kemitix/clover/issue/AbstractBlock.java
@@ -1,6 +1,5 @@
 package net.kemitix.clover.issue;
 
-import lombok.extern.java.Log;
 import net.kemitix.clover.spi.Block;
 import net.kemitix.clover.spi.Drawable;
 import net.kemitix.properties.typed.TypedProperties;
@@ -8,7 +7,6 @@ import net.kemitix.properties.typed.TypedProperties;
 import javax.enterprise.inject.Instance;
 import java.awt.*;
 
-@Log
 public abstract class AbstractBlock implements Block<Graphics2D> {
 
     @Override
@@ -18,8 +16,4 @@ public abstract class AbstractBlock implements Block<Graphics2D> {
 
     protected abstract Instance<? extends Drawable<Graphics2D>> getElements();
 
-    @Override
-    public void logInfo(String message) {
-        log.info(message);
-    }
 }
